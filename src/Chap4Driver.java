a9d82ff028e7622b04a11d14bdb8e0544ba41f88
Mike Swartz <mi.swartz@northeastern.edu>
2022-10-21 20:18:58 -0400

    chap4

---
+++ src/Chap4Driver.java
@@ -0,0 +1,13 @@
+public class Chap4Driver {
+    /**
+     * This is a driver method! When we run this file, Java looks for a method with this name and
+     * signature to run.
+     * @param args
+     */
+    public static void main(String[] args) {
+        Chap4Kitten kittyRufus = new Chap4Kitten("Rufus");
+        new Chap4Kitten("Eloise");
+        System.out.println(kittyRufus.getName());  // -> RufusCuteKitty
+        System.out.println(Chap4Kitten.getKittenCount());  // -> 2
+    }
+}
---
+++ src/Chap4Kitten.java
@@ -0,0 +1,42 @@
+/**
+ * This class represents a kitten, on behalf of chapter 4
+ */
+public class Chap4Kitten {
+    private static int kittenCount = 0;  // this is a static var, useful for keeping track of
+    // info about the class as a whole
+
+    /**
+     * @return Name
+     */
+    public String getName() {
+        return name;
+    }
+
+    private String name; // dynamic var, specific to the instance
+
+    /**
+     *  a constructor! it calls a helper
+     * @param name
+     */
+    public Chap4Kitten(String name) {
+        this.name = cutifyName(name);
+        kittenCount ++;
+    }
+
+    /**
+     * A static method!
+     * @return Number of kitties made
+     */
+    public static int getKittenCount() {
+        return kittenCount;
+    }
+
+    /**
+     * This is a static helper method!
+     * @param name
+     * @return A cuter name
+     */
+    private static String cutifyName(String name) {
+        return name + "CuteKitty";
+    }
+}
